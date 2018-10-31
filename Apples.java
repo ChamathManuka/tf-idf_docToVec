9bbb23964190fb32f13be34e448a7ccc5b638a11
ChamathManuka <manuka.14@cse.mrt.ac.lk>
2018-10-31 13:48:17 +0530

    second commit

---
+++ Apples.java
@@ -0,0 +1,22 @@
+package videoJava;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Tuna tunaObject = new Tuna();
+		System.out.println(tunaObject.toMilitary());
+		tunaObject.setTime(12, 54, 32);
+		System.out.println(tunaObject.toMilitary());
+		System.out.println(tunaObject.toString());
+		
+
+	}
+
+}
+*/
+		
+			
+		}
+	}
+}
